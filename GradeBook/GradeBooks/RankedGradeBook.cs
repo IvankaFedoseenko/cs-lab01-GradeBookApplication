7c2dbaa995b0a64266298cff1a89274bf037d33d
Ivanna Fedoseenko <ivanna.fedoseenko@microsoft.wsei.edu.pl>
2025-03-07 18:45:33 +0100

    not woking version 6

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -14,5 +14,46 @@ namespace GradeBook.GradeBooks
             Type = GradeBookType.Ranked;
             Name = name;
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            foreach (var student in Students)
+            {
+                int countStudents = 0;
+                if(student != null)
+                {
+                    countStudents++;
+                }
+                double topStudents = 1;
+                if(student.AverageGrade > averageGrade)
+                {
+                    topStudents = student.AverageGrade;
+                }
+                else
+                {
+                    topStudents++;
+                }
+                if(topStudents <= countStudents * 20 / 100)
+                {
+                    return 'A';
+                }
+                if(topStudents <= countStudents * 40 / 100)
+                {
+                    return 'B';
+                }
+                if (topStudents <= countStudents * 60 / 100)
+                {
+                    return 'C';
+                }
+                if (topStudents <= countStudents * 80 / 100)
+                {
+                    return 'D';
+                }
+                else
+                {
+                    return 'F';
+                }
+
+            }
+        }
     }
 }
